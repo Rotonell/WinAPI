66a9310a0833837a47e4d4f2c7039c3ae38d5800
Дарья Серова <dashaflay2000@gmail.com>
2025-11-21 11:31:59 +0300

    ComboBox DONE

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -1,4 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<cstdio>
 #include"resource.h"
 
 CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "First", "Combo", "Box" };
@@ -18,22 +20,40 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_INITDIALOG:
 	{
 		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
-		for (int i = 0; i < sizeof(g_sz_VALUES)/ sizeof(g_sz_VALUES[0]); i++)
+		for (int i = 0; i < sizeof(g_sz_VALUES) / sizeof(g_sz_VALUES[0]); i++)
 		{
 			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)g_sz_VALUES[i]);
 		}
 	}
-		break;
+	break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
 		{
 		case IDOK:
-			break;
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			CHAR sz_message[SIZE]{};
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			if (i != CB_ERR)
+			{
+				SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
+				sprintf(sz_message, "Вы выбрали элемент №%i со значением \"%s\".", i, sz_buffer);
+				MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
+			}
+			else
+			{
+				MessageBox(hwnd, "Вы ничего не выбрали", "Info", MB_OK | MB_ICONINFORMATION);
+			}
+		}
+		break;
 		case IDCANCEL:
 			EndDialog(hwnd, 0);
 			break;
 		}
+		break;
 	}
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
